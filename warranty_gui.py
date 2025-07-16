4c1bd2f54b51aa79dbf1b3a843b6cb710b805268
Carter Pylman <carterpylman@gmail.com>
2025-07-16 16:07:49 -0400

    updated gui: added about, how to help, and some variables to prepare for next feature

--- warranty_gui.py
+++ warranty_gui.py
@@ -1,17 +1,21 @@
 import warranty_lookup as wnty
 import tkinter as tk
 from tkinter import filedialog as fd, ttk, messagebox
+from tkinter import StringVar as sv
 import csv
 
 csv_file = None
 tree = None
 table_frame = None
+filter_var = Nonefilter_col_var = None
+all_rows = []
 """
 Download a csv file after inputting it into the "Enter file" button
 
 Add options for different models or manufacturers
 Finish help menu
 Finish about menu
+Add editing function
 Add other QOL changes I can think of
 """
 
@@ -23,7 +27,7 @@ def open_file():
         show_csv_tree(csv_file)
 
 def show_csv_tree(file_path):
-    global tree
+    global tree, all_rows, filter_var, filter_col_var, table_frame
 
     for widget in table_frame.winfo_children():
         widget.destroy()
@@ -63,14 +67,10 @@ def main():
     help_menu.add_command(label="About", command=show_about)
 
     #file menu
-    file_menu = tk.Menu(menu)
-    menu.add_cascade(label="Enter File", menu=file_menu)
-    file_menu.add_command(label="Open", command=open_file)
-
-    #download menu
-    download_menu = tk.Menu(menu)
-    menu.add_cascade(label="Download", menu=download_menu)
-    download_menu.add_command(label="Download CSV", command=download_file)
+    file_menu = tk.Menu(menu, tearoff=0)
+    menu.add_cascade(label="File", menu=file_menu)
+    file_menu.add_command(label="Open File for Warranty Lookup", command=open_file)
+    file_menu.add_command(label="Download Updated File", command=download_file)
 
     #Frame for table
     table_frame = tk.Frame(root)
@@ -95,10 +95,17 @@ def download_file():
         messagebox.showwarning("No File", "Open a CSV file first to download it!")
 
 def show_help():
-    tk.messagebox.showinfo("How to Use", "1 lah blah")
-
+    tk.messagebox.showinfo("How to Use", "1: Create Excel or Sheets file with list of serial numbers you want to look up." \
+    "\n2. Download those files as CSV files." \
+    "\n3. Open the CSV file using File -> Open File for Warranty Lookup." \
+    "\n4. The table will populate on the main screen with the serial numbers and their warranty status." \
+    "\n5. Download the updated file if it is to your liking with File -> Download Updated CSV File")
 def show_about():
-    tk.messagebox.showinfo("About", "blah blah")
+    tk.messagebox.showinfo("About", "This application was created using Python and Tkinter."
+    "\nIt's purpose is to look up Lenovo warranty information from a CSV file containing the serial numbers of devices."
+    "\nThere are some limitations, such as Lenovo devices may have a serial number linked with multiple devices."
+    "\nThis application cannot find those devices correctly, so you must go to Lenovo's website to find them."
+    "\nThis was created by a student from Grand Valley State University and has no affiliation with Lenovo.")
 
 if __name__ == "__main__":
     main()
